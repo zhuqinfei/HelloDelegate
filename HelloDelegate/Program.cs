afd179d89cd081f2aff57392f0d6d9dfe7d29d87
zhuqinfei <1586859762@qq.com>
2024-02-19 16:29:51 +0800

    委托类型的使用

---
+++ HelloDelegate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HelloDelegate
+{
+    class Program
+    {
+        /// <summary>
+        /// 这几种委托方式是最原始的，以后可以用action和func
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //第一种
+            MyDele dele = new MyDele(M1);
+
+            //第二种
+            MyDele2 dele2 = new MyDele2(Add);
+            int res = dele2(100, 200);
+
+            //第三种
+            MyDele3<int> dele3 = new MyDele3<int>(Add);
+            int res2 = dele3(200, 400);
+
+        }
+         
+        static void M1()
+        {
+            Console.WriteLine("M1 is called");
+        }
+        static int Add(int x ,int y)
+        {
+            return x + y;
+        }
+        //第一种：是无返回值委托
+        delegate void MyDele();
+        //第二种：有返回值，并且带参数委托
+        delegate int MyDele2(int a,int b);
+        //第三种：是泛型的委托,带返回值类型和参数类型
+        delegate T MyDele3<T>(T a, T b);
+    }
+}
---
+++ HelloDelegate2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloDelegate2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //无返回值，无参数可以用action
+            Action action = new Action(M1);
+            action();
+
+            //无返回值，有参数可以用action
+            Action<string,string> action2 = new Action<string, string>(SayHello);
+            action2("Tim","Tom");
+            //var action2 = new Action<string, string>(SayHello);
+            //action2("Tim", "Tom");
+
+            //有返回值，有参数可以用action，第一个是返回值类型，第二个和第三个是传入参数类型
+            var func = new Func<double, double, double>(Mul);
+            double res = func(3.0, 4.0);
+            Console.WriteLine(res);
+        }
+
+        static void M1()
+        {
+            Console.WriteLine("M1 is called");
+        }
+
+        static void SayHello(string name1,string name2)
+        {
+            Console.WriteLine($"Hello,{name1}and{name2}!");
+        }
+
+        static double Mul(double x,double y)
+        {
+            return x + y;
+        }
+    }
+}
